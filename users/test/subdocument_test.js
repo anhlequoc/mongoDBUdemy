8c2439567d7b0fc1deab6bee840faa449da2af55
Anh Le <lequocanh88@gmail.com>
2017-12-29 15:25:04 +0700

    complete lecture 49

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -16,7 +16,7 @@ describe('Subdocument', () => {
       })
   });
 
-  it('should allow adding post to current user', (done) => {
+  it('should allow adding post to existing record', (done) => {
     const jane = new User({
       name: 'jane',
       posts: []
@@ -26,11 +26,30 @@ describe('Subdocument', () => {
       .then(() => User.findOne({name: 'jane'}) )
       .then((user) => {
         user.posts.push({ title: 'new post' });
-        return user.save(); // dùng lệnh return để buộc js có kết quả từ hàm user.save() rồi mới chạy dòng lệnh tiếp theo
+        return user.save(); // dùng lệnh return để buộc js có kết quả từ hàm user.save() rồi mới chạy dòng lệnh 31 tiếp theo
       })
       .then(() => User.findOne({ name: 'jane'}) )
       .then((user) => {        
         assert(user.posts[0].title === 'new post');
+        done();
+      })
+  });
+
+  it("should removing subdocument from existing record", (done) => {
+    const joe = new User({
+      name: 'joe',
+      posts: [{title: 'current post'}]
+    });
+
+    joe.save()
+      .then(() => User.findOne({name: 'joe'}))
+      .then((user) => {
+        user.posts[0].remove();
+        return user.save();
+      })
+      .then(() => User.findOne({name: 'joe'}))
+      .then((user) => {
+        assert (user.posts.length === 0);
         done();
       })
   });
